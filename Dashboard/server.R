d8ad814ef5b8214877f722ab45932f08033a55e1
ErykWalter <erykwalter2002@gmail.com>
2023-06-16 00:13:18 +0200

    add: data table with info about the albums

--- Dashboard/server.R
+++ Dashboard/server.R
@@ -5,22 +5,65 @@ library(tidyr)
 library(dplyr)
 library(ggplot2)
 library(plotly)
+library(DT)
+
+showcase.data = get_artist_audio_features("Kendrick Lamar")
 
 Sys.setenv(SPOTIFY_CLIENT_ID = '459c2a8ce83c4d5b8692cd08b60d8cd6')
 Sys.setenv(SPOTIFY_CLIENT_SECRET = '0ac29b401c5947119d7efe12302363a3')
 
 function(input, output, session){
-  generate_graph <- eventReactive(input$generate_btn, {
-    graph1_output <- input$graph1_input
-    df <- get_artist_audio_features(graph1_output)
-    df %>%
-      select(c("track_name", "track_href", "liveness", "energy", "loudness")) %>%
-      ggplot(aes(x = liveness, y = energy, color = loudness, text = track_name)) +
-      geom_point()
+  cached.artist <- reactive({
+    if(!is.null(input$artist.name) && nchar(trimws(input$artist.name)) > 0) {
+      return(get_artist_audio_features(input$artist.name))
+    }
+    return(showcase.data)
   })
   
+  createNiceTable <- function(data) {
+    datatable(
+      data,
+      options = list(
+        dom = 'Bfrtip',
+        buttons = c('copy', 'csv', 'excel', 'pdf', 'print'),
+        paging = TRUE,
+        searching = TRUE,
+        ordering = TRUE,
+        autoWidth = TRUE,
+        filter = 'top'
+      ),
+      class = 'display',
+      extensions = 'Buttons'
+    )
+  }
+
   output$graph1 <- renderPlotly({
-    generate_graph()
+    df <- cached.artist()
+    artist <- df$artist_name[1]
+    df %>%
+      select(c("track_name", "liveness", "energy", "album_name")) %>%
+      rename(Album = album_name, 'Energy'=energy, 'Liveness'=liveness) %>%
+      ggplot(aes(x = Liveness, y = Energy, color = Album, text = track_name)) +
+      geom_point() +
+      ggtitle(paste(artist))
+  })
+  
+  output$text1 <- renderText({
+    df <- cached.artist()
+    
+    df[1]
+    columns <- colnames(df)
+    paste(columns, "\n")
+  })
+  
+  output$table1 <- DT::renderDataTable({
+    df <- cached.artist() %>%
+      select(c("album_name", "danceability", "energy", "tempo", "instrumentalness")) %>%
+      rename(album = album_name) %>%
+      aggregate(. ~ album, FUN = mean) %>% 
+      mutate_if(is.numeric, ~ round(., 2))
+    
+    createNiceTable(df)
   })
   
   country_codes <- read.csv("country_codes.csv", sep = ";")
--- Dashboard/ui.R
+++ Dashboard/ui.R
@@ -5,6 +5,7 @@ library(tidyr)
 library(dplyr)
 library(ggplot2)
 library(plotly)
+library(DT)
 
 header <- dashboardHeader(
   title = "Spotify Dashboard"
@@ -16,13 +17,21 @@ sidebar <- dashboardSidebar(
 
 body <- dashboardBody(
   box(
-    title = "Graph1",
-    textInput("graph1_input", "Enter Text:", value = ""),
+    title = "Artist's songs",
+    textInput("artist.name", "Artist's name:", value = ""),
     actionButton("generate_btn", "Generate Graph"),
     plotlyOutput("graph1")
   ),
   box(
-    title = "Graph2",
+    title = "Artist's discography",
+    verbatimTextOutput("text1")
+  ),
+  box(
+    title = "Songs characteristcs",
+    DT::dataTableOutput("table1")
+  ),
+  box(
+    title = "Popular playlists",
     selectInput("graph2_input", "Select Country:", choices = read.csv("country_codes.csv", sep = ";")$Country),
     uiOutput("graph2")
   )
